4796b87ec3bda568ef1e4b7e700c3c32926e80da
coco-server <cocobird231@gmail.com>
2023-07-24 13:43:51 +0800

    Fix crcmod copy error under setup.py

--- py_chassis/setup.py
+++ py_chassis/setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 
 package_name = 'py_chassis'
 
+pkgFiles = [(os.path.join('lib', dp), [os.path.join(dp, f) for f in fn]) for dp, dn, fn in os.walk(os.path.join(package_name))]
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -13,9 +15,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'launch'), glob('launch/common.yaml')),
-        (os.path.join('lib', package_name), glob(package_name + '/*.py')),
-        (os.path.join('lib', package_name, 'crcmod'), glob(package_name + 'crcmod/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/common.yaml')), 
+        *pkgFiles
     ],
     install_requires=['setuptools'],
     zip_safe=True,
